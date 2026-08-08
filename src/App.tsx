34890a12aca808a37ed559a312978f3341068249
merunhowlader <merunhowlader@gmail.com>
2026-08-08 14:27:16 +0600

    chore : initail project setup wiht vite , tailwind , shadcn/ui

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+// import './App.css';
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-background text-foreground">
+      <h1 className="text-3xl text-blue-900 font-b">Tailwind is working</h1>
+      <Button>Shadcn is working</Button>
+    </div>
+  );
+}
+export default App;
